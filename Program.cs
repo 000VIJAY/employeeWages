c0c0479c6dc4799c34ac979c5324e9b898445f7d
000VIJAY <vijay893689@gmail.com>
2022-06-23 13:37:48 +0530

    Added Employee Wage Per month

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 ﻿int employeeRatePerHr = 20;
 int employeeHrs = 0;
 int employeeWagePerDay = 0;
-Random rnd = new Random();
-int num = rnd.Next(0, 3);
-switch (num)
+int employeeWagePerMonth = 0;
+int maxWorkingDaysInMonth = 20;
+for (int i = 1; i <= maxWorkingDaysInMonth; i++)
 {
-    case 0:
-        employeeHrs = 8;
-        break;
-    case 1:
-        employeeHrs = 4;
-        break;
-    default:
-        employeeHrs = 0;
-        break;
+    Random rnd = new Random();
+    int num = rnd.Next(0, 3);
+    switch (num)
+    {
+        case 0:
+            employeeHrs = 8;
+            break;
+        case 1:
+            employeeHrs = 4;
+            break;
+        default:
+            employeeHrs = 0;
+            break;
+    }
+    employeeWagePerDay = employeeHrs * employeeRatePerHr;
+    Console.WriteLine("employeeWagePerDay : " + employeeWagePerDay);
+    employeeWagePerMonth += employeeWagePerDay;
 }
-employeeWagePerDay = employeeHrs * employeeRatePerHr;
-Console.WriteLine("employeeWagePerDay : " + employeeWagePerDay);
+Console.WriteLine("employeeWagePerMonth : " + employeeWagePerMonth);
