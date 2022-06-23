dedf16c292ad56294d067ca77e26db323bc46df1
000VIJAY <vijay893689@gmail.com>
2022-06-23 12:32:27 +0530

    Added Program for check present or absent using random

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿int isFullTime = 1;
+Random rnd = new Random();
+int num = rnd.Next(0, 2);
+if (num == isFullTime)
+{
+    Console.WriteLine("Present");
+}
+else
+    Console.WriteLine("Absent");
