721c9d33c6aaf4d63020d0d97641d55f8f493417
000VIJAY <vijay893689@gmail.com>
2022-06-23 13:26:24 +0530

    Added Employee Wage Per Day including part time

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 ﻿int isFullTime = 1;
+int isPartTime = 0;
 int employeeRatePerHr = 20;
 int employeeHrs = 0;
 int employeeWagePerDay = 0;
 Random rnd = new Random();
-int num = rnd.Next(0, 2);
+int num = rnd.Next(0, 3);
 if (num == isFullTime)
 {
     employeeHrs = 8;
     Console.WriteLine("Present");
+}
+else if (num == isPartTime)
+{
+    employeeHrs = 4;
+    Console.WriteLine("Part Time");
 }
 else
 {
